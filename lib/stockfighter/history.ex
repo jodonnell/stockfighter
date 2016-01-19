7edc8a15057e58a1605b773ba3fe5f366fa59461
Jacob O'Donnell <jacobodonnell@gmail.com>
2016-01-18 22:23:06 -0500

    made history use an agent

--- lib/stockfighter/history.ex
+++ lib/stockfighter/history.ex
@@ -1,48 +1,53 @@
 defmodule StockFighter.History do
+  use Timex
+
   def initial do
-    %{"lastTrade" => Date.from({{1950, 1, 1}, {0, 0, 0}}), "quotes" => []}
+    Agent.start_link(fn -> %{"lastTrade" => Date.from({{1950, 1, 1}, {0, 0, 0}}), "quotes" => []} end, name: __MODULE__)
   end
 
-  def update_quotes(all_quotes, new_quote) do
+  def quotes do
+    Enum.map(all_quotes["quotes"], fn(my_quote) ->
+      my_quote["last"]
+    end)
+  end
+
+  def update_quotes(new_quote) do
     if all_quotes["lastTrade"] < new_quote["lastTrade"] do
-      %{"lastTrade" => new_quote["lastTrade"], "quotes" => [new_quote | all_quotes["quotes"]]}
-    else
-      all_quotes
+        Agent.update(__MODULE__,  fn dict ->
+          %{"lastTrade" => new_quote["lastTrade"], "quotes" => [new_quote | dict["quotes"]]}
+        end)
     end
+
+    all_quotes
   end
 
-  def get_low(all_quotes) do
-    all_quotes
-    |> quotes
+  def get_low() do
+    quotes
     |> Enum.min
   end
 
-  def get_high(all_quotes) do
-    all_quotes
-    |> quotes
+  def get_high() do
+    quotes
     |> Enum.max
   end
 
-  def get_average(all_quotes) do
-    all_quotes
-    |> quotes
+  def get_average() do
+    quotes
     |> _get_average
   end
 
-  def get_local_average(all_quotes) do
-    my_quotes = all_quotes
-    |> quotes
+  def get_local_average() do
+    quotes
     |> Enum.take(30)
     |> _get_average
+  end
+
+  defp all_quotes do
+    Agent.get(__MODULE__, &(&1))
   end
 
   defp _get_average(my_quotes) do
     Enum.sum(my_quotes) / Enum.count(my_quotes)
   end
-  
-  def quotes(all_quotes) do
-    Enum.map(all_quotes["quotes"], fn(my_quote) ->
-      my_quote["last"]
-    end)
-  end
+
 end
--- test/history_test.exs
+++ test/history_test.exs
@@ -7,50 +7,57 @@ defmodule HistoryTest do
     Date.from({{2015, 6, 24}, {hour, 50, 34}})
   end
 
-  def initial do
-    %{"lastTrade" => test_date(0), "quotes" => []}
+  test "initializes" do
+    StockFighter.History.initial
+    assert StockFighter.History.quotes() == []
   end
 
   test "can add to the history" do
-    all_quotes = StockFighter.History.update_quotes(initial, %{"last" => 200, "lastTrade" => test_date(1)})
-    assert StockFighter.History.quotes(all_quotes) == [200]
+    StockFighter.History.initial
+    StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(1)})
+    assert StockFighter.History.quotes() == [200]
   end
 
   test "doesn't add unless updated" do
-    all_quotes = StockFighter.History.update_quotes(initial, %{"last" => 200, "lastTrade" => test_date(1)})
-    assert StockFighter.History.quotes(all_quotes) == [200]
+    StockFighter.History.initial
+    StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(1)})
+    assert StockFighter.History.quotes() == [200]
 
-    all_quotes = StockFighter.History.update_quotes(all_quotes, %{"last" => 300, "lastTrade" => test_date(1)})
-    assert StockFighter.History.quotes(all_quotes) == [200]
+    StockFighter.History.update_quotes(%{"last" => 300, "lastTrade" => test_date(1)})
+    assert StockFighter.History.quotes() == [200]
 
-    all_quotes = StockFighter.History.update_quotes(all_quotes, %{"last" => 400, "lastTrade" => test_date(2)})
-    assert StockFighter.History.quotes(all_quotes) == [400, 200]
+    StockFighter.History.update_quotes(%{"last" => 400, "lastTrade" => test_date(2)})
+    assert StockFighter.History.quotes() == [400, 200]
   end
 
   test "get min" do
-    all_quotes = StockFighter.History.update_quotes(initial, %{"last" => 200, "lastTrade" => test_date(1)})
-    |> StockFighter.History.update_quotes(%{"last" => 400, "lastTrade" => test_date(2)})
-    assert StockFighter.History.get_low(all_quotes) == 200
+    StockFighter.History.initial
+    StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(1)})
+    StockFighter.History.update_quotes(%{"last" => 400, "lastTrade" => test_date(2)})
+    assert StockFighter.History.get_low() == 200
   end
 
   test "get max" do
-    all_quotes = StockFighter.History.update_quotes(initial, %{"last" => 200, "lastTrade" => test_date(1)})
-    |> StockFighter.History.update_quotes(%{"last" => 400, "lastTrade" => test_date(2)})
-    assert StockFighter.History.get_high(all_quotes) == 400
+    StockFighter.History.initial
+    StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(1)})
+    StockFighter.History.update_quotes(%{"last" => 400, "lastTrade" => test_date(2)})
+    assert StockFighter.History.get_high() == 400
   end
 
   test "get average" do
-    all_quotes = StockFighter.History.update_quotes(initial, %{"last" => 100, "lastTrade" => test_date(1)})
-    |> StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(2)})
-    |> StockFighter.History.update_quotes(%{"last" => 300, "lastTrade" => test_date(3)})
-    assert StockFighter.History.get_average(all_quotes) == 200
+    StockFighter.History.initial
+    StockFighter.History.update_quotes(%{"last" => 100, "lastTrade" => test_date(1)})
+    StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(2)})
+    StockFighter.History.update_quotes(%{"last" => 300, "lastTrade" => test_date(3)})
+    assert StockFighter.History.get_average() == 200
   end
 
   test "get local average" do
-    all_quotes = StockFighter.History.update_quotes(initial, %{"last" => 100, "lastTrade" => test_date(1)})
-    |> StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(2)})
-    |> StockFighter.History.update_quotes(%{"last" => 300, "lastTrade" => test_date(3)})
-    assert StockFighter.History.get_local_average(all_quotes) == 200
+    StockFighter.History.initial
+    StockFighter.History.update_quotes(%{"last" => 100, "lastTrade" => test_date(1)})
+    StockFighter.History.update_quotes(%{"last" => 200, "lastTrade" => test_date(2)})
+    StockFighter.History.update_quotes(%{"last" => 300, "lastTrade" => test_date(3)})
+    assert StockFighter.History.get_local_average() == 200
   end
   
 end
